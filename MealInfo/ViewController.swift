21951148633f8e4c936209416cf96df7c874e11f
saitejabadugu <bsaiteja10@gmail.com>
2023-03-22 19:35:12 +0530

    Initial setup

---
+++ MealInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealInfo
+//
+//  Created by Keerthi Devipriya(kdp) on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
