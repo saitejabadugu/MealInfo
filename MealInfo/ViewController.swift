8c89ffc3cada1e65554e95d8cf229163afe40439
saitejabadugu <bsaiteja10@gmail.com>
2023-03-27 00:05:08 +0530

    done categories api and data setup

--- MealInfo/ViewController.swift
+++ MealInfo/ViewController.swift
@@ -11,17 +11,60 @@ class ViewController: UIViewController {
 
     var categoryModel: MealCategoryModel?
     
+    var categoryListTableview: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .singleLine
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getCategoryModel()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.setUpTableView()
+            self.setUpUI()
+            self.setupAutoLayout()
+        }
     }
     
+    func setUpTableView() {
+        categoryListTableview.delegate = self
+        categoryListTableview.dataSource = self
+        categoryListTableview.register(UITableViewCell.self, forCellReuseIdentifier: "categories")
+
+    }
+    
+    func setUpUI() {
+        view.backgroundColor = .white
+        view.addSubview(categoryListTableview)
+    }
+    
+    func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            categoryListTableview.leftAnchor.constraint(equalTo: view.leftAnchor),
+            categoryListTableview.rightAnchor.constraint(equalTo: view.rightAnchor),
+            categoryListTableview.topAnchor.constraint(equalTo: view.topAnchor),
+            categoryListTableview.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+        ])
+    }
+
     func getCategoryModel() {
         Api.getMealCategories { categories in
             self.categoryModel = categories
         }
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.categoryModel?.meals.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categories", for: indexPath)
+        cell.textLabel?.text = categoryModel?.meals[indexPath.row].strCategory
+        return cell
+    }
+}
